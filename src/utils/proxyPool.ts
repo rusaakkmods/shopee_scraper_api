f33c3c286975dc463031a0026d4a3a6822d70abf
Pria Jatnika <65021505+rusaakkmods@users.noreply.github.com>
2025-06-05 11:20:57 +0700

    Update proxyPool.ts
    redacting username and pass

--- src/utils/proxyPool.ts
+++ src/utils/proxyPool.ts
@@ -4,8 +4,8 @@ dotenv.config()
 // This module exports a function to retrieve proxy connection details from environment variables.
 // It is designed to be used in applications that require proxy settings, such as web scraping or API requests.
 export default function getProxy() {
-  const username = process.env.BRIGHT_DATA_USERNAME || 'brd-customer-hl_16acc6e1-zone-residential_proxy1'
-  const password = process.env.BRIGHT_DATA_PASSWORD || 'aj1g3ort351c'
+  const username = process.env.BRIGHT_DATA_USERNAME || '**********'
+  const password = process.env.BRIGHT_DATA_PASSWORD || '**********'
   const host = process.env.BRIGHT_DATA_HOST || 'brd.superproxy.io'
   const port = parseInt(process.env.BRIGHT_DATA_PORT || '33335')
 
